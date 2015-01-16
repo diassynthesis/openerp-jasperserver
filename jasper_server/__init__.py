7d6b9880a68b8dcf4155cc1d58704dd3753e34d9
diassynthesis <diassynthesis@gmail.com>
2015-01-16 16:02:24 +0100

    Update __init__.py

--- jasper_server/__init__.py
+++ jasper_server/__init__.py
@@ -21,7 +21,7 @@
 #    along with this program.  If not, see [http://www.gnu.org/licenses/].
 #
 ##############################################################################
-
+import common
 import ir_actions_wizard
 import obj_server
 import obj_document
